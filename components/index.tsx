9c5eae54e51a714f4b81fa9e67ce09ada1d145b3
onlyfoot <onlyfoot.agency@gmail.com>
2026-02-05 12:45:00 -0300

    Delete components/index.tsx

--- components/index.tsx
+++
@@ -1,6 +0,0 @@
-// components/index.tsx
-import AuthLayout from './AuthLayout';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-
-export { AuthLayout, Login, Register };
